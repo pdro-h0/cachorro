bb8df56c58c6661c2498919fff145b3f2b88ce54
pdro-h0 <pedro.pho792@gmail.com>
2023-01-08 19:41:20 -0300

    Começando o site com mobile first

---
+++ index.js
@@ -0,0 +1,85 @@
+/* SELECAO DE ELEMENTOS================== */
+const menuMobileHambur = document.querySelector('.menu-mobile')
+const menuMobileX = document.querySelector('.menu-mobile-x')
+const menuMobileOpen = document.querySelector('.mobile')
+const menuLinks = document.querySelectorAll(".menu-open  li a")
+const bodyMenuMobile = document.querySelector('.menu-open')
+const header = document.querySelector('header')
+const headerHeith = header.offsetHeight
+
+menuLinks.forEach((e) =>{
+    e.addEventListener('click', ()=>{
+        setTimeout(() => {
+            menuMobileOpen.style.right = '-100vw'
+        }, 200);
+    })
+})
+
+menuMobileHambur.addEventListener('click', ()=>{
+    menuMobileOpen.style.right = 0
+
+    setTimeout(() => {
+        bodyMenuMobile.style.opacity = 1
+    }, 10);
+})
+
+menuMobileX.addEventListener('click', () =>{
+    menuMobileOpen.style.right = '-100vw'
+
+    setTimeout(() => {
+        bodyMenuMobile.style.opacity = 0
+    }, 10);
+})
+
+
+window.addEventListener('scroll', headerScroll)
+
+
+
+
+function headerScroll(){
+    if(window.scrollY > headerHeith){
+        header.classList.add('menu-scroll')
+    }else{
+        header.classList.remove('menu-scroll')
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* SWIPER */
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    keyboard: true,
+    breakpoints: {
+        770: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
+})
